ccb7cc47d1667f1aede32f05fb1ed11c66184118
sophilophie <sophia@sophiamckell.com>
2023-04-21 19:27:14 -0600

    add basic user functionality

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { User, UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+
+  constructor(private userService: UserService) {}
+
+  @Get()
+  getUsers(): User[] {
+    return this.userService.getUsers();
+  }
+
+  @Get(':userId')
+  getUser(@Param() params: {userId: number}): User {
+    return this.userService.getUser(params.userId);
+  }
+
+  @Post()
+  postUser(@Req() request: Request): User {
+    return this.userService.postUser(request.body);
+  }
+
+  @Put(':userId')
+  putUser(@Param() params: {userId: number}, @Req() request: Request): User {
+    return this.userService.putUser(params.userId, request.body);
+  }
+
+  @Delete(':userId')
+  deleteUser(@Param() params: {userId: number}): User {
+    return this.userService.deleteUser(params.userId);
+  }
+}
--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -1,4 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import * as _ from 'lodash';
+
+export interface User {
+  id: number;
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private users: User[] = [
+    {id: 0, username: 'testUser0', firstName: 'user', lastName: 'name', email: 'user@name0.com', password: 'someHash'},
+    {id: 1, username: 'testUser1', firstName: 'user', lastName: 'name', email: 'user@name1.com', password: 'someHash'},
+    {id: 2, username: 'testUser2', firstName: 'user', lastName: 'name', email: 'user@name2.com', password: 'someHash'},
+    {id: 3, username: 'testUser3', firstName: 'user', lastName: 'name', email: 'user@name3.com', password: 'someHash'},
+    {id: 4, username: 'testUser4', firstName: 'user', lastName: 'name', email: 'user@name4.com', password: 'someHash'},
+    {id: 5, username: 'testUser5', firstName: 'user', lastName: 'name', email: 'user@name5.com', password: 'someHash'},
+    {id: 6, username: 'testUser6', firstName: 'user', lastName: 'name', email: 'user@name6.com', password: 'someHash'}
+  ];
+
+  public getUsers(): User[] {
+    return this.users;
+  }
+
+  public getUser(userId: number): User {
+    return this.users[userId];
+  }
+
+  public postUser(user: User): User {
+    this.users.push(user);
+    return this.users[user.id];
+  }
+
+  public putUser(userId: number, user: User): User {
+    this.users[userId] = user;
+    return this.users[userId];
+  }
+
+  public deleteUser(userId: number): User {
+    const tempUser = _.cloneDeep(this.users[userId]);
+    delete this.users[userId];
+    this.users = _.omitBy(this.users, _.isNil) as User[];
+    return tempUser;
+  }
+}
