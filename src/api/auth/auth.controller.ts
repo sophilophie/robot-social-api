0c7ded4d6ab4441fbed740d6e04e2d384ae1938e
sophilophie <sophia@sophiamckell.com>
2023-04-25 15:45:37 -0600

    add refresh token endpoint

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SkipJwtAuth } from '../../common/decorators/skip-jwt.decorator';
+import { RefreshUserDto } from '../user/dto/refresh-user.dto';
 import { JwtResponse } from './auth-types';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
@@ -13,5 +14,11 @@ export class AuthController {
   @Post('login')
   login(@Body() loginDto: LoginDto): Promise<JwtResponse> {
     return this.authService.login(loginDto);
+  }
+
+  @SkipJwtAuth()
+  @Post('refresh')
+  refresh(@Body() refreshDto: RefreshUserDto): Promise<JwtResponse> {
+    return this.authService.refresh(refreshDto);
   }
 }
